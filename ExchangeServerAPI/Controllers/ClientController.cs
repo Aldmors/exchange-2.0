cdb9d4bfedb5c5782accb1810b97a106e4712619
Kacper Satora <kacpersatora02@gmail.com>
2024-05-13 13:04:14 +0200

    Minimal DB setup + sln

---
+++ ExchangeServerAPI/Controllers/ClientController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExchangeServerAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExchangeServerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ExchangeServerContext _context;
+
+        public ClientController(ExchangeServerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = client.ClientId }, client);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(int id, Client client)
+        {
+            if (id != client.ClientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Clients.Any(e => e.ClientId == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ExchangeServerAPI/Migrations/20240513105829_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExchangeServerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    SecondName = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    Email = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    Phone = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    Address = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    StreetNumber = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    City = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    State = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    PostalCode = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    Country = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true),
+                    Notes = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
